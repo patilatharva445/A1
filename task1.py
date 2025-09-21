0eb4dc330a51a6287dc07a905b24de8a83ebe2f1
Atharva Patil <patilatharva445@gmail.com>
2025-09-21 11:54:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a 1st number: "));
+b=int(input("Enter a 2nd number: "));
+
+print('Addition:',a+b);
+print('Subtraction:',a-b);
+print('Multiplication:',a*b);
+print('Division:',a/b);
+
+print('Thank You!');
+
---
+++ task2.py
@@ -0,0 +1,7 @@
+fname=input('Enter your first name: ');
+lname=input('Enter your last name: ');
+
+a=fname;
+b=lname;
+
+print('Hello',a,b , 'Welcome to Python Programming!!');
